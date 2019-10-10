bfc4768f664d3c3369e9f50a81251c252bad7a94
sachin saini <sachinsaini@Administrator.local>
2019-10-10 18:34:51 +0530

    Updates

---
+++ precache-manifest.388e8366de703fd28f206f7a11a45ee8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f4563c6744c4763d5705141438bab6",
+    "url": "/manageCampaign/index.html"
+  },
+  {
+    "revision": "ea912ee15eb607df54db",
+    "url": "/manageCampaign/static/css/main.bbd40828.chunk.css"
+  },
+  {
+    "revision": "3dac6e4d651060fc3c5a",
+    "url": "/manageCampaign/static/js/2.23ef046e.chunk.js"
+  },
+  {
+    "revision": "ea912ee15eb607df54db",
+    "url": "/manageCampaign/static/js/main.fe3c3c75.chunk.js"
+  },
+  {
+    "revision": "f575da9ea33b5abca080",
+    "url": "/manageCampaign/static/js/runtime-main.30d9611a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/manageCampaign/static/media/logo.25bf045c.svg"
+  }
+]);
